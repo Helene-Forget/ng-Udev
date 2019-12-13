5ec74e1ab2d31819dcc171cffb24a3e7aee4aa27
Forget <helene.jourdain75@gmail.com>
2019-12-13 17:17:57 +0100

    add header,library et library-item components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule} from '@angular/material';
+import { LibraryComponent } from './library/library.component';
+import { LibraryItemComponent } from './library-item/library-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LibraryComponent,
+    LibraryItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/library.ts
@@ -0,0 +1,21 @@
+import {Book} from './book';
+
+export const library: Book[] = [
+  {
+    id: '1',
+    title: 'blablabla',
+    author: 'JahnJahn'
+  },
+  {
+    id: '2',
+    title: 'blobloblo',
+    author: 'JohnJohn'
+  },
+  {
+    id: '3',
+    title: 'bliblibli',
+    author: 'JihnJihn'
+  }
+];
+
+
